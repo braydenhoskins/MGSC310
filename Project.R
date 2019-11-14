4e27bee13dbd01c0dd41d13f1f3265e2d583c510
nestradarand <noah.estrada456@gmail.com>
2019-11-14 11:05:41 -0800

    fixed genres and added some graphs

--- Project.R
+++ Project.R
@@ -20,23 +20,19 @@ steam$price <- steam$price *1.28
 ####got rid of cariables that wont really help us
 steam<- subset(steam, select = -c(appid,english,steamspy_tags,
                                   name,release_date,
-                                  platforms))
+                                  platforms,publisher,developer))
 
 ####I got rid of all the ';' delimited variables and instead assigned them a single value for that columne
 steam$genres <- do.call('rbind',strsplit(as.character(steam$genres), ';', fixed=TRUE))[,1]
 steam$categories <- do.call('rbind',strsplit(as.character(steam$categories), ';', fixed=TRUE))[,1]
-steam$publisher <- do.call('rbind',strsplit(as.character(steam$publisher), ';', fixed=TRUE))[,1]
-steam$developer <- do.call('rbind',strsplit(as.character(steam$developer), ';', fixed=TRUE))[,1]
 ###turn them all into factors
-steam$developer <- as.factor(steam$developer)
 steam$categories <- as.factor(steam$categories)
 steam$genres <- as.factor(steam$genres)
-steam$publisher <- as.factor(steam$publisher)
 
 
 ###removing outliers
 steam <- steam[steam$average_playtime < 100000,]
-steam <- steam[steam$price <100,]
+steam <- steam[steam$price <50,]
 steam <- steam[steam$average_playtime < 40000,]
 steam <- steam[steam$negative_ratings < 2e+05,]
 steam <- steam[steam$positive_ratings < 1e+06,]
@@ -66,8 +62,15 @@ steam$categories <- ifelse(steam$categories == "Single-player","SinglePlayer",
                                                                                                  ifelse(steam$categories == "Steam Leaderboards","Steam",
                                                                                                         ifelse(steam$categories == "Steam Achievements","Steam","Other")))))))))))))
 steam$categories <- as.factor(steam$categories)
+unique(steam$categories)
+
 
 unique(steam$categories)
+###reducing the number of genres
+steam$genres <- fct_lump(steam$genres,n = 5)
+unique(steam$genres)
+
+
 ###creates variable successful game to help predict by (I was thinking we could 
 #use this as our predicted variable)(1 is a successful game - over 1 million sold,0 is anything less)
 steam$successfulGame <- ifelse(steam$owners == "10000000-20000000",1,
@@ -78,7 +81,7 @@ steam$successfulGame <- ifelse(steam$owners == "10000000-20000000",1,
                                                            ifelse(steam$owners == "2000000-5000000",1,
                                                                   ifelse(steam$owners == "1000000-2000000",1,0)))))))
 steam$successfulGame <- as.factor(steam$successfulGame)
-steam <- subset(steam, select = -c(owners,developer))
+steam <- subset(steam, select = -c(owners))
 
 library(summarytools)
 descr(steam)
@@ -121,6 +124,14 @@ ggplot(steam,aes(x = price,y = successfulGame)) +
   geom_point()+
   labs(title = "Successful Game plotted Against Average Playtime",
        x = "Price of a Game (dollars)",y = "Successful Game (1 = Yes, 0 = No)")
+
+boxplot(steam$price~steam$categories,
+        main = "Boxplot of Price By Category",
+        xlab = "Categories",
+        ylab = "Price (dollars)")
+ggplot(steam,aes(x = genres,y = price)) + geom_boxplot() + 
+  labs(title = "Boxplots of Price By Genre",x = "Genres",y = "Price (dollars)")
+
 
 summary(steam)
 
